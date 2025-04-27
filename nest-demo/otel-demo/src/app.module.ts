81a082472d19066ab532e347eeda8390145a6b2a
jgkim <jgkim999@gmail.com>
2025-04-27 17:55:57 +0900

    otel

--- nest-demo/otel-demo/src/app.module.ts
+++ nest-demo/otel-demo/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
+import { OpenTelemetryModule } from 'nestjs-otel';
+
+const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
+  metrics: {
+    hostMetrics: true,
+    apiMetrics: {
+      enable: true,
+    },
+  },
+});
 
 @Module({
-  imports: [],
+  imports: [OpenTelemetryModuleConfig],
   controllers: [AppController, MoviesController],
   providers: [AppService],
 })
