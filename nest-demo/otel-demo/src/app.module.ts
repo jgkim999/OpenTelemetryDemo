5be5f2da005612f780e236df8d909924805ef124
jgkim <jgkim999@gmail.com>
2025-05-02 20:42:26 +0900

    metic, trace

--- nest-demo/otel-demo/src/app.module.ts
+++ nest-demo/otel-demo/src/app.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
-import { OpenTelemetryModule } from 'nestjs-otel';
 import { ConfigModule } from '@nestjs/config';
+import { OpenTelemetryModule } from 'nestjs-otel';
 
 const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
   metrics: {
     hostMetrics: true,
     apiMetrics: {
       enable: true,
+      defaultAttributes: {
+        // You can set default labels for api metrics
+        custom: 'nest',
+      },
+      ignoreRoutes: ['/favicon.ico'], // You can ignore specific routes (See https://docs.nestjs.com/middleware#excluding-routes for options)
+      ignoreUndefinedRoutes: false, //Records metrics for all URLs, even undefined ones
+      prefix: 'demo', // Add a custom prefix to all API metrics
     },
   },
 });
