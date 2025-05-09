ed21e95835489254cd9d71259b4ddcaabf0627c7
jgkim <jgkim@neowiz.com>
2025-05-09 16:21:03 +0900

    feat: :construction:  프로젝트 초기 설정 및 Gradle 구성 추가, 기본 애플리케이션 및 테스트 클래스 생성

---
+++ java-spring-boot-movie-otel-demo/src/test/java/com/jgkim/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgkim.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
